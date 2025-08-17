83bd044343270725099e608f9705b45657cbbebf
ZackWilde27 <zackwilde27@gmail.com>
2025-08-16 20:28:27 -0600

    Revert "Added source code"
    This reverts commit ec4063b651425be10d937d599e11d1ecdb36f90d.

--- source/FlatOut2_ZacksSSR/FlatOut2_ZacksSSR/BuildLinked.ps1
+++
@@ -1,9 +0,0 @@
-# Set Working Directory
-Split-Path $MyInvocation.MyCommand.Path | Push-Location
-[Environment]::CurrentDirectory = $PWD
-
-Remove-Item "$env:RELOADEDIIMODS/FlatOut2_ZacksSSR/*" -Force -Recurse
-dotnet publish "./FlatOut2_ZacksSSR.csproj" -c Release -o "$env:RELOADEDIIMODS/FlatOut2_ZacksSSR" /p:OutputPath="./bin/Release" /p:ReloadedILLink="true"
-
-# Restore Working Directory
-Pop-Location
